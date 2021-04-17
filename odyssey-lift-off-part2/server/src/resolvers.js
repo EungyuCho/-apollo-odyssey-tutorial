ae7774d6c6e3c7679c9900b6eda6a824e51a1b92
jay <cho2304@naver.com>
2021-04-17 23:30:14 +0900

    :bug: fix query bug

--- odyssey-lift-off-part2/server/src/resolvers.js
+++ odyssey-lift-off-part2/server/src/resolvers.js
@@ -1,15 +1,13 @@
 const resolvers = {
   Query: {
-    // retuns an array of Tracks that will be used to populate
-    // the homepage grid of our web client
+    // returns an array of Tracks that will be used to populate the homepage grid of our web client
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
   },
-
   Track: {
-    author: ({ authorId }, _, { dataSource }) => {
-      return dataSource.trackAPI.getAuthor(authorId);
+    author: ({ authorId }, _, { dataSources }) => {
+      return dataSources.trackAPI.getAuthor(authorId);
     },
   },
 };
